d2ae6ede61ecf804e9acd046a299027efb65bd71
newlook2024 <abdulvahob2022@gmail.com>
2024-06-08 09:21:20 +0500

    level function added

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 let coin = document.querySelector(".coin");
 let container = document.querySelector(".count-container");
 let limit = document.querySelector("#limit");
-let earnedCoins = document.querySelector('.earned-coins')
+let earnedCoins = document.querySelector(".earned-coins");
 
-limit.textContent = 1000;
+limit.textContent = 5000;
 let minusCount = 1;
-let limitEqual;
 
 setInterval(() => {
   if (limit.textContent < 1000) {
@@ -14,7 +13,7 @@ setInterval(() => {
       limit.textContent = 1000;
     }
   }
-}, 1500);
+}, 1000);
 
 // setInterval(() => {
 //   limitEqual = limit.textContent - minusCount;
@@ -22,22 +21,19 @@ setInterval(() => {
 
 coin.addEventListener("click", (e) => {
   let counter = document.createElement("h1");
-  earnedCoins.textContent = earnedCoins.textContent*1 + minusCount*1
   if (limit.textContent > 0) {
+    earnedCoins.textContent = earnedCoins.textContent * 1 + minusCount * 1;
     let cursorX = e.clientX;
     let cursorY = e.clientY;
     counter.style.left = `${cursorX - 10}px`;
     counter.style.top = `${cursorY - 50}px`;
     limit.textContent = limit.textContent - minusCount;
     setTimeout(() => {
-      counter.style.top = "-100px";
+      counter.style.top = "-30px";
       counter.style.opacity = "0";
     }, 1);
     counter.classList.add("counter");
     counter.textContent = `+${minusCount}`;
-  }
-  if (limit.textContent < limitEqual) {
-    limit.textContent = 0;
   }
   setInterval(() => {
     if (limit.textContent * 1 < 0) {
@@ -57,4 +53,30 @@ coin.addEventListener("click", (e) => {
       coin.style.opacity = "0.5";
     }
   }, 200);
+});
+
+let levelOpen = document.querySelector("#level-up-opener");
+let levelUp_modal = document.querySelector(".modal-container");
+let modalCloser = document.querySelector("#modalCloser");
+let levelUpBtn = document.querySelector("#level-up");
+let coinForLevel = document.querySelector('#coinForLevel')
+let level = document.querySelector('#next-level')
+let level__title = document.querySelector('.level-title')
+
+levelOpen.addEventListener("click", () => {
+  levelUp_modal.style.display = "flex";
+});
+
+modalCloser.addEventListener("click", () => {
+  levelUp_modal.style.display = "none";
+});
+
+levelUpBtn.addEventListener("click", () => {
+  if(coinForLevel.textContent*1 <=  earnedCoins.textContent*1){
+    level.textContent = level.textContent*1 + 1
+    earnedCoins.textContent = earnedCoins.textContent*1 - coinForLevel.textContent*1
+    coinForLevel.textContent = coinForLevel.textContent*1 + 5000
+    level__title.textContent = `Your level ${level.textContent*1 -1}`
+    minusCount = minusCount +1 
+  }
 });
